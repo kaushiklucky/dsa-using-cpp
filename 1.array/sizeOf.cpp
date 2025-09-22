66aa1e4e64dc7d05fc3c8279d231111957addf0b
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-09-22 12:44:33 +0530

    first commit

---
+++ 1.array/sizeOf.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int size,n;
+    int arr[] = {1,2,3,4,5,6,7,8,9,0,1,34,56,78,90,67,89,45,67};
+    size = sizeof(arr)/sizeof(arr[0]);
+    cout<<size;
+}
