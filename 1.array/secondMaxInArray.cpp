cc390faa6806697353ab7fb02d5dff25b4b1b722
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-09-22 12:43:53 +0530

    first commit

---
+++ 1.array/secondMaxInArray.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+      int n;
+      cout<<"Enter the size of array : ";
+      cin>>n;
+      cout<<"Enter "<<n<<" numbers in array : ";
+      int arr[n], i;
+      //input
+      for(i=0; i<=n-1; i++){
+            cin>>arr[i];
+      }
+      //search
+      int max = INT_MIN;
+      for(i=0; i<=n-1; i++){
+            if(max<arr[i]) max = arr[i]; 
+      }
+      
+      int smax = INT_MIN;
+      for(i=0; i<=n-1; i++){
+            if(arr[i]!=max && smax<arr[i]) smax = arr[i];
+      }
+      cout<<"largest element "<<max<<endl;
+      cout<<"second largest element "<<smax;
+}
