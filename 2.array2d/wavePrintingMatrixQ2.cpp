8f38acab923ee9c09ed519e0363da7061ca19b13
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-10-01 17:43:56 +0530

    first commit

---
+++ 2.array2d/wavePrintingMatrixQ2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+      int m,n;
+      cout<<"Enter row no. : ";
+      cin>>m;
+      cout<<"Enter colom no. : ";
+      cin>>n;
+      cout<<"Enter elements in array : ";
+      int arr[m][n];
+      for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                  cin>>arr[i][j];
+            }
+      }
+      
+      for(int i=0; i<m; i++){
+            if(i%2==0){
+                  for(int j=n-1; j>=0; j--){
+                        cout<<arr[i][j]<<" ";
+                  }
+                  cout<<endl;
+            }
+            else{
+                  for(int j=0; j<n; j++){
+                        cout<<arr[i][j]<<" ";
+                  }
+                  cout<<endl;
+            }
+      }
+}
